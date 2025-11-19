e6f4dc4eb8dd26d096b262d73fd487a8181ac8d6
Joel Boersma <joelboersma@gmail.com>
2025-11-18 20:50:57 -0800

    simplify loading while actually rendering loading
    This reverts commit e7f43e564a27397a80f6639d6ee13d8ad2c56f35.

--- main.go
+++ main.go
@@ -100,21 +100,15 @@ func renderResultsView(searchResponse *customsearch.Search, query string, pageNu
 }
 
 func showLoading(pageNumber int) {
-	loadingText := "Loading..."
-	textView := tview.NewTextView().SetText(loadingText)
+	textView := tview.NewTextView().
+		SetText("Loading...").
+		SetTextAlign(tview.AlignCenter)
 
-	// Horizontally center the loading indicator in a column flex
-	colFlex := tview.NewFlex().
-		SetDirection(tview.FlexColumn).
-		AddItem(tview.NewBox(), 0, 1, false).
-		AddItem(textView, len(loadingText), 1, true).
-		AddItem(tview.NewBox(), 0, 1, false)
-
-	// Vertically center the column flex in a row flex
+	// Vertically center in a row flex
 	rowFlex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
 		AddItem(tview.NewBox(), 0, 1, false).
-		AddItem(colFlex, 1, 1, true).
+		AddItem(textView, 1, 1, true).
 		AddItem(tview.NewBox(), 0, 1, false)
 
 	title := fmt.Sprint("Results - Page ", pageNumber)
